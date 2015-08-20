b2bb40e3ab9c0ce5aad7e6985fe78514573a1c69
Stefan <szdralje@gmail.com>
2015-08-20 14:33:10 -0500

    dataset annotation

---
+++ R/datasets.R
@@ -0,0 +1,22 @@
+#' SNP set.
+#'
+#' Genotype information for C. elegans wild isolates. SNPs used from RADseq data set but 
+#' includes all 124 wild isolates from whole-genome sequencing project
+#' @name snps
+#' @format A data frame with snps as rows and columns as strains:
+#' \describe{
+#'   \item{CHROM}{chromosome in roman numerals}
+#'   \item{POS}{physical position in genome}
+#'   \item{AB1}{first strain in collection, followed by all other strains}
+#'   ...
+#' }
+NULL
+
+#' Relatedness matrix
+#'
+#' Kinship matrix generated using the \code{a.mat} function in the rrBLUP package. 
+#' Whole-genome SNP data was used to generate the relatedness of the strains in this file.
+#' @name kinship
+#' @format A Strain x Strain data frame for 124 strains
+NULL
+
