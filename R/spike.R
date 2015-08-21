0d9adfcd238105ee7af613e9c9684498a324b242
Tyler Shimko <TShimko126@gmail.com>
2015-08-21 14:49:04 -0500

    Update spike to handle multiple QTL

--- R/spike.R
+++ R/spike.R
@@ -3,33 +3,62 @@
 #' This function generates fake data for testing GWAS mappings, with the option
 #' to generate QTL. 
 #'
-#' @param meadiff The abolute difference in means for the two allele populations
-#' @param sd The standard deviation for both populations
 #' @param snps The snp set being used
 #' @param snpindex The index of the snp upon which to spike the effect
 #' @return A phenotype data frame with the fake spiked data, ready for process_pheno
 #' @export
 
 
-spike <- function(meandiff, sd, snps, snpindex) {
-    set.seed(17)
-    
-    strains <- colnames(snps)[-c(1, 2)]
-    trait_values <- seq_along(strains)
-    snps <- data.frame(snps)
-    for(i in seq_along(strains)) {
-        if(snps[snpindex, i + 2] == 0) {
-            trait_values[i] <- rnorm(1, mean = meandiff, sd = sd)
-        } else {
-            trait_values[i] <- rnorm(1, mean = 0, sd = sd)
+spike <- function(snps, snpindex) {
+    if (length(snpindex) == 1) {
+        set.seed(17)
+        
+        strains <- colnames(snps)[-c(1, 2)]
+        trait_values <- seq_along(strains)
+        snps <- data.frame(snps)
+        for(i in seq_along(strains)) {
+            if(snps[snpindex, i + 2] == 0) {
+                trait_values[i] <- rnorm(1, mean = 20, sd = 1)
+            } else {
+                trait_values[i] <- rnorm(1, mean = 0, sd = 1)
+            }
         }
+        
+        trait_values <- rbind(trait_values)
+        
+        pheno <- data.frame("test", trait_values)
+        pheno[,2:ncol(pheno)] <- lapply(pheno[,2:ncol(pheno)], as.numeric)
+        
+        colnames(pheno) <- c("trait", strains)
+        return(pheno)
+    } else {
+        set.seed(17)
+        strains <- colnames(snps)[-c(1, 2)]
+        trait_values <- rep(0, times = length(strains))
+        snps <- data.frame(snps)
+        
+        for (snp in snpindex) {
+            meandiff <- rnorm(1, 30, sd = 5)
+            sd <- rnorm(1, mean = 3, sd = 1)
+            for(i in seq_along(strains)) {
+                if(is.na(snps[snp, i + 2])){
+                    trait_values[i] <- trait_values[i] + 0
+                } else {
+                    if(snps[snp, i + 2] == 0) {
+                        trait_values[i] <- trait_values[i] + rnorm(1, mean = meandiff, sd = sd)
+                    } else {
+                        trait_values[i] <- trait_values[i] - rnorm(1, mean = meandiff, sd = sd)
+                    }
+                }
+            }
+        }
+        
+        trait_values <- rbind(trait_values)
+        
+        pheno <- data.frame("test", trait_values)
+        pheno[,2:ncol(pheno)] <- lapply(pheno[,2:ncol(pheno)], as.numeric)
+        
+        colnames(pheno) <- c("trait", strains)
+        return(pheno)
     }
-    
-    trait_values <- rbind(trait_values)
-    
-    pheno <- data.frame("test", trait_values)
-    pheno[,2:ncol(pheno)] <- lapply(pheno[,2:ncol(pheno)], as.numeric)
-    
-    colnames(pheno) <- c("trait", strains)
-    return(pheno)
 }
