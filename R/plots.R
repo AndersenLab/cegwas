dc2bb21ff1b6536e1c756aa85915fdc5e971847e
Daniel Cook <danielecook@gmail.com>
2015-10-25 22:07:32 -0500

    Simplify manplot function.

--- R/plots.R
+++ R/plots.R
@@ -10,53 +10,10 @@
 
 
 manplot <- function(plot_df) {
-  
-  if(length(unique(plot_df$trait)) == 1){
-    
-    plot_df %>%
-      dplyr::distinct(marker) %>%
-      dplyr::mutate(n_snps = n())%>%
-      ggplot2::ggplot(.) +
-      ggplot2::aes(x = POS/1e6, y = log10p) +
-      ggplot2::scale_color_manual(values = c("black","blue","red")) +
-      ggplot2::geom_rect(ggplot2::aes(xmin = startPOS/1e6, 
-                             xmax = endPOS/1e6, 
-                             ymin = 0, 
-                             ymax = Inf, 
-                             fill = "blue", 
-                             alpha=.1), 
-                         color = NA)+
-      ggplot2::geom_hline(ggplot2::aes(yintercept = BF),
-                          color = "gray", 
-                          alpha = .75,  
-                          size = 1)+
-      ggplot2::geom_hline(ggplot2::aes(yintercept = -log10(.05/n_snps)),
-                          color = "gray", 
-                          alpha = .75,  
-                          size = 1)+
-      ggplot2::geom_point( ggplot2::aes(color= factor(aboveBF)) ) +
-      ggplot2::facet_grid( . ~ CHROM, scales = "free_x" ) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(axis.text.x = ggplot2::element_text(size=24, face="bold", color="black"),
-                     axis.text.y = ggplot2::element_text(size=24, face="bold", color="black"),
-                     axis.title.x = ggplot2::element_text(size=24, face="bold", color="black", vjust=-.3),
-                     axis.title.y = ggplot2::element_text(size=24, face="bold", color="black"),
-                     strip.text.x = ggplot2::element_text(size=24, face="bold", color="black"),
-                     strip.text.y = ggplot2::element_text(size=16, face="bold", color="black"),
-                     plot.title = ggplot2::element_text(size=24, face="bold", vjust = 1),
-                     legend.position="none",
-                     panel.background = ggplot2::element_rect( color="black",size=1.2),
-                     strip.background = ggplot2::element_rect(color = "black", size = 1.2)) +
-      ggplot2::labs(x = "Genomic Position (Mb)",
-                    y = expression(-log[10](p)),
-                    title = unique(plot_df$trait))
-  } 
-  else
-  {
     plot_traits <- unique(plot_df$trait)
-    lapply(1:length(plot_traits), function(i) {
-      p <- plot_df %>%
-        dplyr::filter(trait == plot_traits[i]) %>%
+    plots <- lapply(plot_traits, function(i) {
+        plot_df %>%
+        dplyr::filter(trait == i) %>%
         dplyr::distinct(marker) %>%
         dplyr::mutate(n_snps = n())%>%
         ggplot2::ggplot(.) +
@@ -92,12 +49,11 @@ manplot <- function(plot_df) {
                        strip.background = ggplot2::element_rect(color = "black", size = 1.2)) +
         ggplot2::labs(x = "Genomic Position (Mb)",
                       y = expression(-log[10](p)),
-                      title = plot_traits[i])
-      p
+                      title = i)
     })
-    
+    plots
   }
-}
+
 
 
 #' PxG plot
