75e398e4993a240ceb7dfb25c0926c21500de978
Stefan <szdralje@gmail.com>
2015-10-27 15:28:39 -0500

    color up to four strains in pxg function

--- R/plots.R
+++ R/plots.R
@@ -63,50 +63,76 @@ manplot <- function(plot_df) {
 #'
 #' @param plot_df the output from the \code{gwas_mappings} function. 
 #' @param loc custom location to output the pxg plot. Specified as CHROM:POS (e.g. II:1023423)
+#' @param color_strains character vector containing strains to color in plot. Default is c("N2","CB4856")
 #' @return Ouput is a ggplot object facetted by peak SNP (if there are multiple peaks in a mapping). 
 #' Genotypes are encoded as REF or ALT and are on the x-axis. Phenotypes are on y-axis.
 #' @export
 
-pxg_plot <- function(plot_df, loc = NA){
-    plot_traits <- unique(plot_df$trait)
-    plots <- lapply(plot_traits, function(x) {
-      plot_peak <- plot_df %>%
-        na.omit() %>%
-        dplyr::filter(trait == x) %>%
-        dplyr::distinct(strain, value, peakPOS) %>%
-        dplyr::select(strain, value, CHROM, POS = peakPOS, -allele) %>%
-        dplyr::mutate(chr_pos = paste(CHROM, POS, sep="_"))
-      
-      if (is.na(loc)) {
-        loc <- plot_peak %>% dplyr::select(CHROM, POS) %>% 
-          dplyr::distinct() %>% 
-          dplyr::transmute(CHROM_POS = paste0(CHROM, ":",POS))
-      }
-      
-      snpeff(loc[[1]], severity = c("MODIFIER", "LOW", "MODERATE", "HIGH")) %>%
-        dplyr::select(CHROM, POS, strain, GT) %>%
-        dplyr::distinct() %>%
-        dplyr::left_join(plot_peak) %>%
-        dplyr::distinct(strain, value, POS) %>%
-        dplyr::filter(!is.na(GT)) %>%
-        ggplot2::ggplot(., ggplot2::aes(x = GT, y = value, fill = GT)) + 
-        ggplot2::scale_fill_brewer(palette = "Set1") +
-        ggplot2::geom_boxplot() +
-        ggplot2::theme_bw() +
-        ggplot2::geom_jitter(alpha = .7) +
-        ggplot2::facet_grid(.~chr_pos, scales = "free") +
-        ggplot2::theme(axis.text.x = ggplot2::element_text(size=24, face="bold", color="black"),
-                       axis.text.y = ggplot2::element_text(size=24, face="bold", color="black"),
-                       axis.title.x = ggplot2::element_text(size=24, face="bold", color="black", vjust=-.3),
-                       axis.title.y = ggplot2::element_text(size=24, face="bold", color="black"),
-                       strip.text.x = ggplot2::element_text(size=24, face="bold", color="black"),
-                       strip.text.y = ggplot2::element_text(size=16, face="bold", color="black"),
-                       plot.title = ggplot2::element_text(size=24, face="bold", vjust = 1),
-                       legend.position="none",
-                       panel.background = ggplot2::element_rect( color="black",size=1.2),
-                       strip.background = ggplot2::element_rect(color = "black", size = 1.2)) +
-        ggplot2::labs(y = "Phenotype", x = "Genotype", title = x)
-    })
+pxg_plot <- function(plot_df, loc = NA, color_strains = c("N2","CB4856")){
+  plot_traits <- unique(plot_df$trait)
+  plots <- lapply(plot_traits, function(x) {
+    plot_peak <- plot_df %>%
+      na.omit() %>%
+      dplyr::filter(trait == x) %>%
+      dplyr::distinct(strain, value, peakPOS) %>%
+      dplyr::select(strain, value, CHROM, POS = peakPOS, -allele) %>%
+      dplyr::mutate(chr_pos = paste(CHROM, POS, sep="_"))
+    
+    if (is.na(loc)) {
+      loc <- plot_peak %>% dplyr::select(CHROM, POS) %>% 
+        dplyr::distinct() %>% 
+        dplyr::transmute(CHROM_POS = paste0(CHROM, ":",POS))
+    }
+    
+    to_plot <- snpeff(loc[[1]], severity = c("MODIFIER", "LOW", "MODERATE", "HIGH")) %>%
+      dplyr::select(CHROM, POS, strain, GT) %>%
+      dplyr::distinct() %>%
+      dplyr::left_join(plot_peak) %>%
+      dplyr::distinct(strain, value, POS) %>%
+      dplyr::filter(!is.na(GT)) 
+    
+    if (!unique(is.na(color_strains))) {
+      to_plot <- to_plot %>%
+        dplyr::mutate(colors = ifelse(strain %in% color_strains, strain, "aa_ignore"))%>%
+        dplyr::arrange(colors)
+    } else {
+      to_plot <- to_plot %>%
+        dplyr::mutate(colors = "aa_ignore")
+    }
+    
+    to_plot %>%
+      ggplot2::ggplot(., ggplot2::aes(x = GT, y = value, fill = GT)) + 
+      ggplot2::scale_fill_brewer(palette = "Set1") +
+      ggplot2::geom_boxplot(outlier.colour = NA) +
+      ggplot2::theme_bw() +
+      ggplot2::geom_jitter(ggplot2::aes(color = colors,
+                                        size = colors)) +
+      ggplot2::scale_color_manual(values = c("black", "pink", "brown", "orange", "green"),
+                                  labels = c("Other", 
+                                             unique(to_plot$colors)[2], 
+                                             unique(to_plot$colors)[3], 
+                                             unique(to_plot$colors)[4], 
+                                             unique(to_plot$colors)[5]))+
+      ggplot2::scale_size_manual(values = c(2, 4, 4, 4, 4),
+                                 labels = c("Other", 
+                                            unique(to_plot$colors)[2], 
+                                            unique(to_plot$colors)[3], 
+                                            unique(to_plot$colors)[4], 
+                                            unique(to_plot$colors)[5])) +
+      # ggplot2::scale_color_brewer(palette = "Dark2", name = "Specified\nStrains") +
+      ggplot2::facet_grid(.~chr_pos, scales = "free") +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(size=24, face="bold", color="black"),
+                     axis.text.y = ggplot2::element_text(size=24, face="bold", color="black"),
+                     axis.title.x = ggplot2::element_text(size=24, face="bold", color="black", vjust=-.3),
+                     axis.title.y = ggplot2::element_text(size=24, face="bold", color="black"),
+                     strip.text.x = ggplot2::element_text(size=24, face="bold", color="black"),
+                     strip.text.y = ggplot2::element_text(size=16, face="bold", color="black"),
+                     plot.title = ggplot2::element_text(size=24, face="bold", vjust = 1),
+                     # legend.position="none",
+                     panel.background = ggplot2::element_rect( color="black",size=1.2),
+                     strip.background = ggplot2::element_rect(color = "black", size = 1.2)) +
+      ggplot2::labs(y = "Phenotype", x = "Genotype", title = x)
+  })
   plots
 }
 
