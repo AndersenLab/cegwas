a754ee51adff18e813ba09e0840430cfdf5f4d06
Daniel Cook <danielecook@gmail.com>
2016-01-24 23:26:36 -0600

    fix get_strain_isotype()

--- R/datasets.R
+++ R/datasets.R
@@ -7,8 +7,7 @@
 
 get_strain_isotype <- function() {
   readr::read_tsv("http://storage.googleapis.com/andersen_lab_strains/processed/strain_isotype.tsv") %>%
-    dplyr::arrange(strain, isotype) %>%
-    dplyr::select(strain, isotype, latitude, longitude, sequenced, prev_names, warning_msg, alternative_name)
+    dplyr::arrange(strain, isotype)
 }
 
 #' SNP set (Dataset)
