4096195c862827ec4e8cf55b3ecd397f9b8c7e75
Daniel Cook <danielecook@gmail.com>
2016-03-27 22:02:40 -0500

    update kinship/snps

--- inst/build_data.R
+++ inst/build_data.R
@@ -31,9 +31,9 @@ load("data/mapping_snps.rda")
 
 
 wb_build <- 245
-#vcf_path <- "~/Dropbox/Andersenlab/Reagents/WormReagents/Variation/Andersen_VCF/WI.20160326.impute.vcf.gz"
-#kinship <- generate_kinship(vcf_path)
-#snps <- generate_mapping(vcf_path)
+vcf_path <- "~/Dropbox/Andersenlab/Reagents/WormReagents/Variation/Andersen_VCF/WI.20160326.impute.vcf.gz"
+kinship <- generate_kinship(vcf_path)
+snps <- generate_mapping(vcf_path)
 
 # Save Datasets
 devtools::use_data(kinship, snps, strain_isotype, mapping_snps, gene_ids, wb_build, internal = T, overwrite = T)
